49d926c09d5e034fdf519a8d3319cf353d262394
Phinni <93153106+Phinni@users.noreply.github.com>
2022-07-23 21:06:06 +0200

    imported new components WeatherForecast and WeatherInfo to Weather.js and tested Weather.css

---
+++ src/WeatherForecast.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function WeatherForecast() {
+  return (
+    <div className="WeatherForecast">
+      <h3>Forecasts</h3>
+    </div>
+  );
+}
---
+++ src/WeatherInfo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function WeatherInfo() {
+  return (
+    <div className="WeatherInfo">
+      <h2>Weather Info Today</h2>
+    </div>
+  );
+}
